51f3ecfce7cd790c7b3bce63a246c0a6d0daf4b3
riy8n <mahafujhossainriyan02@gmail.com>
2025-07-22 14:08:16 +0600

    Added the navbar color chnage

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -64,7 +64,7 @@ useEffect(()=>{
     </>
     return (
       
-          <div className="px-3  mt-2 rounded-xl sticky top-0 z-50 navbar bg-gradient-to-r from-[#7832e9] via-[#d272e6] to-[#cc78e6] shadow-sm">
+          <div className="px-3  mt-2 rounded-xl sticky top-0 z-50 navbar bg-gradient-to-r from-[#2e2e4b] via-[#4560aa] to-[#1b67c4]  shadow-sm">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
